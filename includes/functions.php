8b532dab3e7287a60ce445dc1d8de68baf8a5f35
Hugo Saner <hugo@sanerdesign.com>
2013-08-06 11:04:19 +0100

    Changed comment count code to stop kicking up strict errors

--- includes/functions.php
+++ includes/functions.php
@@ -370,7 +370,8 @@ endif;
 function responsive_comment_count( $count ) {  
 	if ( ! is_admin() ) {
 		global $id;
-		$comments_by_type = &separate_comments(get_comments('status=approve&post_id=' . $id));
+        $comments = get_comments('status=approve&post_id=' . $id);
+		$comments_by_type = separate_comments( $comments );
 		return count($comments_by_type['comment']);
 	} else {
 		return $count;
