8ee022f3acb29808c0f7977a55544faf0e4d9c6f
Ulrich Pogson <grapplerulrich@gmail.com>
2014-02-24 20:55:53 +0100

    Add skip-link-focus-fix.js

--- js-dev/responsive-scripts.js
+++ js-dev/responsive-scripts.js
@@ -517,3 +517,24 @@ jQuery(document).ready(function () {
 // Target your #container, #wrapper etc.
 	jQuery("#wrapper").fitVids();
 });
+
+// skip-link-focus-fix.js https://github.com/Automattic/_s/blob/master/js/skip-link-focus-fix.js
+( function() {
+	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+
+	if ( ( is_webkit || is_opera || is_ie ) && 'undefined' !== typeof( document.getElementById ) ) {
+		var eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
+		window[ eventMethod ]( 'hashchange', function() {
+			var element = document.getElementById( location.hash.substring( 1 ) );
+
+			if ( element ) {
+				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
+					element.tabIndex = -1;
+
+				element.focus();
+			}
+		}, false );
+	}
+})();
